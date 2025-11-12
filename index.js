d0d2babd42b519f7f4a2f54838fbb07ccd578298
mdsujonislam666c <mdsujonislam666>
2025-11-13 00:02:38 +0600

    this is my first code push for server side products informations

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://islamic-corner:VgKNrJ8rmC2EYw5g@cluster0.skswfst.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('smart server is running')
+})
+
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('islamic_db');
+        const productsCollection = db.collection('products');
+        const usersCollection = db.collection('users');
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+
+            const email = req.body.email;
+            const query = { email: email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                res.send({ message: 'user already exits. do not need to insert again' })
+            }
+            else {
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result);
+            }
+        })
+
+        app.get('/products', async (req, res) => {
+            // console.log(req.query);
+            // const email = req.query.email;
+            // const query = {}
+            // if(email){
+            //     query.email = email;
+            // }
+
+            const cursor = productsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/allProducts', async(req, res) =>{
+            const cursor = productsCollection.find().sort({posted_date: -1})
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/latest-products', async (req, res) => {
+            const cursor = productsCollection.find().sort({posted_date: -1}).limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/products', async (req, res) => {
+            const newProducts = req.body;
+            const result = await productsCollection.insertOne(newProducts);
+            res.send(result);
+        })
+
+        app.patch('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: {
+                    name: updateProduct.name,
+                    price: updateProduct.price
+                }
+            }
+            const result = await productsCollection.updateOne(query, update);
+            res.send(result);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log('Pinged your deployment. you successfully connected to MongoDB!');
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Smart server is running on port: ${port}`);
+})
